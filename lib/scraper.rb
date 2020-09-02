1e7fe8f66e7788e3b0bee9ef716afeaf8499afee
daniel campo <campo_daniel94@yahoo.com>
2020-09-02 20:46:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,9 @@ class Scraper
     end
   end
   
-  
+  def get_courses
+  self.get_page.css(".post")
+end
   
     def get_page
        Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
